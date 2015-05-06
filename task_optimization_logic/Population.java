b253ee809277d6261e7d657eaf012ceef0418e14
Daniel Cortes <dcortes@konradgroup.com>
2015-05-06 09:26:16 -0600

    [develop] Updated population class logic. Added readme file.

--- task_optimization_logic/Population.java
+++ task_optimization_logic/Population.java
@@ -1,5 +1,39 @@
 package task_optimization_logic;
 
 public class Population {
+	Process[] processes;
 	
+	public Pupulation(int populationSize, boolean initialise) {
+		processes = new Process[populationSize];
+		
+		if (initialise) {
+			for (int i = 0; i < populationSize(); i++) {
+                Process process = new Process();
+                process.generateIndividual();
+                saveProcess(i, process);
+            }
+		}
+	}
+	
+    public void saveProcess(int index, Process process) {
+        processes[index] = process;
+    }
+    
+    public Process getProcess(int index) {
+        return processes[index];
+    }
+
+    public Process getFittest() {
+        Process fittest = processes[0];
+        for (int i = 1; i < populationSize(); i++) {
+            if (fittest.getFitness() <= getProcess(i).getFitness()) {
+                fittest = getProcess(i);
+            }
+        }
+        return fittest;
+    }
+
+    public int populationSize() {
+        return processes.length;
+    }
 }
---
+++ task_optimization_logic/README.md
@@ -0,0 +1,16 @@
+# Task Manager Optimization using Genetic Algorithm
+===============
+
+This is an adaptation of the traveling salesman problem (http://www.theprojectspot.com/tutorial-post/applying-a-genetic-algorithm-to-the-travelling-salesman-problem/5). 
+In this case, the traveler is seen as a process of tasks of e.g. a manufacturing company. Each task has a duration and also there's time to pass from one task to another. 
+However, this time is static. So, a reference matrix is used to store the durations. The matrix has NxN dimentions.
+
+# Example of the matrix:
+
+	A	B	C	D
+A	0	2	5	1
+B	3	0	4	2
+C	1	3	0	6
+D	4	8	2	0
+
+This can be read as: "To go from task A to task B it takes 2 hours". Going from task A to task A takes 0 hours, and so on.
